8c6914f8b152c2e4b89145fae979a31827ddfd2c
Sebastian Valdes <sebastian.valdes.contreras@gmail.com>
2018-06-04 15:16:56 -0400

    commit de proyecto ejemplo sin codigo

---
+++ src/main/java/com/prueba/pruebaJenkins/PruebaJenkinsApplication.java
@@ -0,0 +1,12 @@
+package com.prueba.pruebaJenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaJenkinsApplication.class, args);
+	}
+}
